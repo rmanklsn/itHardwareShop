59b08395d73e0e3cff059af7866337518502865d
Роман Колесников <kolerom@gmail.com>
2024-03-25 14:43:05 +0100

    creaated Hardware class for site shop

---
+++ domain/Store/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Store
+{
+    public class Hardware
+    {
+        public string Title { get; }
+
+        public int Id { get;  } 
+
+        public Hardware (int id, string title)
+        {
+            Title = title;
+            Id = id;
+        }
+    }
+}

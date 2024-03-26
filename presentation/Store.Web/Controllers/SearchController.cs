46515661ae40fe8c5cf27a512e78889c13155a23
Роман Колесников <kolerom@gmail.com>
2024-03-27 00:04:39 +0100

    web page for search works

---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly iHardwareRepository itHardwareRepository;
+
+        public SearchController(iHardwareRepository itHardwareRepository)
+        {
+            this.itHardwareRepository = itHardwareRepository;
+        }
+
+        public IActionResult Index(string query)
+        {
+            var itHardWare = itHardwareRepository.GetAllByTitle(query);
+
+            return View(itHardWare);
+        }
+    }
+}
--- presentation/Store.Web/Program.cs
+++ presentation/Store.Web/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Store;
+using Store.Memory;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<iHardwareRepository, HardWareTitlesRepository>();
 
 var app = builder.Build();
 

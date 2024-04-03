8d022b2a66a6e1782b28afe22838f5c35c05c489
Роман Колесников <kolerom@gmail.com>
2024-04-03 21:32:36 +0200

    added Description for hardware and price, price is a decimal type as this type is widely used for baking soft.

--- domain/Store/Hardware.cs
+++ domain/Store/Hardware.cs
@@ -8,10 +8,16 @@ namespace Store
         public int Id { get; }
         public string Title { get; }
 
-        public Hardware(int id, string title)
+        public string Description { get; }
+
+        public decimal Price { get; }
+
+        public Hardware(int id, string title, string description, decimal price)
         {
             Id = id;
             Title = title;
+            Description = description;
+            Price = price;
         }
     }
 }
--- infrastructure/Store.Memory/HardWareTitlesRepository.cs
+++ infrastructure/Store.Memory/HardWareTitlesRepository.cs
@@ -7,11 +7,11 @@ namespace Store.Memory
     {
         private readonly Hardware[] IT_Hardware = new[]
         {
-            new Hardware(1,"Amd 3000G"),
-            new Hardware(2,"RTX 4080"),
-            new Hardware(3,"Amd ryzen 7 5700"),
-            new Hardware(4,"CPU for servers"),
-            new Hardware(5, "SSD for desctops and laptops"),
+            new Hardware(1,"Amd 3000G", "processor", 10.07m),
+            new Hardware(2,"RTX 4080", "gaming videocard", 12.00m),
+            new Hardware(3,"Amd ryzen 7 5700", "8 cores cpu", 100.1m),
+            new Hardware(4,"intel Xeon", "cpu for servers", 1000.00m),
+            new Hardware(5, "Sandisk","SSD for desctops and laptops", 50.74m),
         };
 
         public Hardware[] GetAllByTitle(string titlePart)
